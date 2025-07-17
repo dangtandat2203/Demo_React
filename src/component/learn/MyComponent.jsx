d1a7204711ee501ff52c9aafa6e815a2ada46627
Dang Tan Dat <dangtandat832@gmail.com>
2025-07-17 11:40:22 +0700

    video 31

--- src/component/learn/MyComponent.jsx
+++ src/component/learn/MyComponent.jsx
@@ -2,9 +2,23 @@
 import './style.css';
 
 const MyComponent = () => {
+    //kiểu dữ liệu nguyên thủy
+    //const a = "Hello Hello"; //string 
+    //const a = 25; //number
+    //const a= true; //boolean
+    //const a = undefined;
+    //const a = null;
+
+    //kiểu dự liệu không nguyên thủy
+    const a = [1, 2, 3]; //array
+    // const a = {
+    //     name: " dangtandat",
+    //     age: 21
+    // }
     return (
         <>
-            <div className='abc'>Hello dangtandat update 1 </div>
+            <div className='abc'>{JSON.stringify(a)} dangtandat update 1 </div>
+            <div>{console.log("Dangtandat")}</div>
         </>
     );
 }
