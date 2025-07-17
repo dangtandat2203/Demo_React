fff9ccdd6ed6066c752b3c8c2d40099313cb2216
Dang Tan Dat <dangtandat832@gmail.com>
2025-07-17 13:09:58 +0700

    video 34

--- src/App.jsx
+++ src/App.jsx
@@ -5,17 +5,25 @@ import reactLogo from './assets/react.svg'
 const App = () => {
 
 
-
+  const name = "Dang Tan Dat aka DTD";
+  const age = 21;
+  const data = {
+    address: "Tay Ninh",
+    country: "Viet Nam"
+  }
   return (
     <>
       <div className="todo-container">
         <div className="todo-title">Todo List</div>
         <TodoNew />
-        <TodoData />
+        <TodoData
+          name={name}
+          age={age}
+          data={data}
+        />
         <div className='todo-image'>
           <img src={reactLogo} className='logo' />
         </div>
-
       </div>
 
     </>
--- src/components/todo/TodoData.jsx
+++ src/components/todo/TodoData.jsx
@@ -1,6 +1,15 @@
-const TodoData = () => {
+const TodoData = (props) => {
+    //props là một object {}
+    // {
+    //     name: "Dang Tan Dat",
+    //     age: 21,
+    //     data: {}
+    // }
+    const { name, age, data } = props
+    console.log(">>> check props: ", props)
     return (
         <div className='todo-data'>
+            <div>My name is {props.name}</div>
             <div>Learning React</div>
             <div>Watching Youtube</div>
         </div>
