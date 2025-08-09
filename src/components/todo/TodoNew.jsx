dbfeb4eafbc210644ae06067f9bd91f0c80e333c
Dang Tan Dat <dangtandat832@gmail.com>
2025-08-09 20:06:33 +0700

    Video 36

--- src/components/todo/TodoNew.jsx
+++ src/components/todo/TodoNew.jsx
@@ -1,12 +1,23 @@
 const TodoNew = (props) => {
     console.log(">>> check point:", props)
     const { addNewToDo } = props;
-    addNewToDo("DangTanDat")
+    // addNewToDo("DangTanDat")
+
+    const handClick = () => {
+        alert("click me")
+    }
+    const handleOnChange = (name) => {
+        console.log("handleOnChange", name)
+    }
     return (
         <>
             <div className='todo-new'>
-                <input type="text" />
-                <button>Add</button>
+                <input type="text"
+                    onChange={(event) => handleOnChange(event.target.value)}
+                />
+                <button style={{ cursor: "pointer " }}
+                    onClick={handClick}
+                >Add</button>
             </div>
         </>
     )
